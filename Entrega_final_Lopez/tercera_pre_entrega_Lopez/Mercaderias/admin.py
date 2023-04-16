7efafd2d39ffc5ab8feb147b4354a3630698c0f7
Mariano Lopez <marianolopez7749@gmail.com>
2023-04-16 10:20:24 -0300

    prueba  commit

--- Entrega_final_Lopez/tercera_pre_entrega_Lopez/Mercaderias/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Productos, Proveedores, Compras, Stock
-
-# Register your models here.
-admin.site.register(Productos)
-admin.site.register(Proveedores)
-admin.site.register(Compras)
-admin.site.register(Stock)
